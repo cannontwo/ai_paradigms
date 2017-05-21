8aea5f9166a9d222858acff7972fad86b801d06b
cannon <cannon10100@gmail.com>
2017-05-20 21:03:57 -0500

    Finished Ch. 4

---
+++ gps/blocksworld_domain.lisp
@@ -0,0 +1,43 @@
+;; The Blocks World Domain
+
+(in-package :gps)
+
+
+;; Functions
+(defun make-block-ops (blocks)
+  (let ((ops nil))
+    (dolist (a blocks)
+      (dolist (b blocks)
+        (unless (equal a b)
+          (dolist (c blocks)
+            (unless (or (equal c a) (equal c b))
+              (push (move-op a b c) ops)))
+          (push (move-op a 'table b) ops)
+          (push (move-op a b 'table) ops))))
+    ops))
+
+(defun move-op (a b c)
+  "Make an operator to move A from B to C."
+  (op `(move ,a from ,b to ,c)
+      :preconds `((space on ,a) (space on ,c) (,a on ,b))
+      :add-list (move-ons a b c)
+      :del-list (move-ons a c b)))
+
+(defun move-ons (a b c)
+  (if (eq b 'table)
+    `((,a on ,c))
+    `((,a on ,c) (space on ,b))))
+
+;; SUSSMAN ANOMALY
+(use (make-block-ops '(a b c)))
+
+(setf start '((c on a) (a on table) (b on table) (space on c)
+                       (space on b) (space on table)))
+
+;; Goal ordering 1
+(gps start '((a on b) (b on c)))
+
+;; Goal ordering 2
+(gps start '((b on c) (a on b)))
+
+
--- gps/gps_version_2.lisp
+++ gps/gps_version_2.lisp
@@ -42,10 +42,24 @@
   (cond ((member-equal goal state) state)
         ((member-equal goal goal-stack) nil)
         (t (some #'(lambda (op) (apply-op state goal op goal-stack))
-                 (find-all goal *ops* :test #'appropriate-p)))))
+                 (appropriate-ops goal state)))))
+
+(defun appropriate-ops (goal state)
+  "Return a list of appropriate operators, sorted by the 
+   number of unfulfilled preconditions."
+  (sort (copy-list (find-all goal *ops* :test #'appropriate-p)) #'<
+        :key #'(lambda (op)
+                 (count-if #'(lambda (precond)
+                               (not (member-equal precond state)))
+                           (op-preconds op)))))
 
 (defun achieve-all (state goals goal-stack)
-  "Try to achieve each goal, then make sure they still hold."
+  "Try to achieve each goal, trying several orderings."
+  (some #'(lambda (goals) (achieve-each state goals goal-stack))
+            (orderings goals)))
+
+(defun achieve-each (state goals goal-stack)
+  "Achieve each goal, and make sure that they still hold at the end."
   (let ((current-state state))
     (if (and (every #'(lambda (g)
                         (setf current-state
@@ -53,6 +67,11 @@
                     goals)
              (subsetp goals current-state :test #'equal))
       current-state)))
+
+(defun orderings (l)
+  (if (> (length l) 1)
+      (list l (reverse l))
+      (list l)))
 
 (defun member-equal (item list)
   (member item list :test #'equal))
